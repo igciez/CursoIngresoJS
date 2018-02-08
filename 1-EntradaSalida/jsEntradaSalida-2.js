4551c8a8544a0a51eeaf375d14d2c14cbd51352e
igciez <36281553+igciez@users.noreply.github.com>
2018-02-08 20:36:25 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,16 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
+	// Defino una variable
+	var nombre;
+	//alert(nombre);
+	nombre=prompt("ingrese su nombre","natalia");
+	alert(nombre);
+	/*
+	nombre="Ignacio";
+	alert(nombre);
+	alert("nombre");
+	*/ 
+	//var person = prompt("Please enter your name", "Harry Potter");
 }
 

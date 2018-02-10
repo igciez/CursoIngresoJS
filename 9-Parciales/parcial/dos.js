15c94a76fedc4a2eb8b5757e5c84d9780964379d
igciez <36281553+igciez@users.noreply.github.com>
2018-02-09 22:13:38 -0300

    hasta el dos

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,4 +1,12 @@
 function Mostrar()
 {
-  
+  var importe;
+  var importefinal;
+
+  importe=prompt("Ingrese el importe");
+  importe=parseInt(importe);
+  importefinal=importe+(importe*0.21);
+
+ document.getElementById('importeFinal').value=importefinal;
+
 }
--- 9-Parciales/parcial/uno.js
+++ 9-Parciales/parcial/uno.js
@@ -1,5 +1,12 @@
 
 function Mostrar()
 {
+	var base;
+	var resultado;
+
+	base=document.getElementById('laBase').value;
+	base= parseInt(base);
+	resultado=base*4;
+	alert("El perimetro del cuadrado es " + resultado);
 
 }

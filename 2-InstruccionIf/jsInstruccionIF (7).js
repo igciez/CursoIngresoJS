469f9159cc2225c01af6e12b3ad0552bea059e42
igciez <36281553+igciez@users.noreply.github.com>
2018-02-15 12:51:19 -0300

    corregir

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,10 +1,23 @@
 function Mostrar()
 {	
 	var edad;
+	var estadoCivil
+	var soltero; 
+
 
 	edad=document.getElementById('edad').value;
 	edad=parseInt(edad);
-//tomo la edad  
+	document.getElementById('estadoCivil').value=estadoCivil;
+	
+	
+	
+
+	if (estadoCivil!= 'soltero' && edad<18) {
+
+		alert('Es muy pequeno para no ser soltero');
+	}
+
+ 
 
 	
 

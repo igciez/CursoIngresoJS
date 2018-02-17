606d03a7910f5216db02688f3f189e98660a07ef
igciez <36281553+igciez@users.noreply.github.com>
2018-02-16 21:55:54 -0300

    Switch completado

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -4,7 +4,29 @@ function Mostrar()
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
+switch(mesDelAño){
+
+	case "Abril":
+	case "Junio":
+	case "Septiembre":
+	case "Noviembre":{
+		alert("Este mes contiene 30 días.");break;
+	}
+
+	case "Febrero":{
+		alert("Este mes contiene 28 días.");break;
+	}
+	case "Enero":
+	case "Marzo":
+	case "Mayo":
+	case "Julio":
+	case "Agosto":
+	case "Octubre":
+	case "Diciembre":{
+		alert("Este mes contiene 31 días.");break;
+	}
+
+}	
 	
 
 

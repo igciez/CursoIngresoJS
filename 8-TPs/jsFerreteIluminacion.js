979ebb7f64b62344108c39a048a26d17a9dfc8e5
igciez <36281553+igciez@users.noreply.github.com>
2018-02-22 14:53:55 -0300

    Add files via upload

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -12,15 +12,78 @@ function CalcularPrecio ()
 {
  	var lamparas;
  	var importe;
+ 	var marca;
+ 	var precioDescuento;
 
  	lamparas=document.getElementById('Cantidad').value;
  	importe=lamparas*35;
+ 	marca=document.getElementById('Marca').value;
 
- 	if (lamparas>=6) {
+ 	
+ 	switch (lamparas) {
+
+ 		case "5":{
+ 			if (marca=='ArgentinaLuz') {
+ 				document.getElementById("precioDescuento").value=(importe-(importe*0.4));
+ 			}
+ 			if (marca!='ArgentinaLuz') {
+ 				document.getElementById("precioDescuento").value=(importe-(importe*0.3));
+			};break;
+ 		}
+ 		case "4":{
+ 			if (marca=='ArgentinaLuz'|| marca=='FelipeLamparas') {
+ 				document.getElementById("precioDescuento").value=(importe-(importe*0.25));
+ 			}
+ 			if (marca!='ArgentinaLuz'|| marca!='FelipeLamparas') {
+ 				document.getElementById("precioDescuento").value=(importe-(importe*0.20));
+ 			};break;
+ 		}
+ 		case "3":{
+ 			if (marca=='ArgentinaLuz') {
+ 				document.getElementById("precioDescuento").value=(importe-(importe*0.15));
+			}
+			if (marca=='FelipeLamparas') {
+				document.getElementById("precioDescuento").value=(importe-(importe*0.10));
+			}
+			if (marca!='ArgentinaLuz'|| marca!='FelipeLamparas') {
+			document.getElementById("precioDescuento").value=(importe-(importe*0.05));	
+			};break;
+ 		}
+ 		default/*case 6:{}*/:{
+			document.getElementById("precioDescuento").value=(importe-(importe*0.5));break; 
+		}
+ 	}
+
+ 	precioDescuento=document.getElementById("precioDescuento").value;
+ 	precioDescuento=parseInt(precioDescuento);
+
+ 	if (precioDescuento>120) {
+
+ 		alert("IIBB Usted pago " +(precioDescuento+(precioDescuento*.1)) + " siendo "+(precioDescuento*.1)+ " el impuesto que se pagó");
+
+	}
+
+
+
+
+ 	/*if (lamparas>=6) {
 
  		document.getElementById("precioDescuento").value=(importe-(importe*0.5));
 
  	}
+
+ 	if (lamparas==5&&marca=='ArgentinaLuz'){
+
+ 		document.getElementById("precioDescuento").value=(importe-(importe*0.4));
+ 	}
+
+ 	if (lamparas==5&&marca!='ArgentinaLuz') {
+
+ 		document.getElementById("precioDescuento").value=(importe-(importe*0.3));
+ 	}*/
+
+
+
 
 
  	

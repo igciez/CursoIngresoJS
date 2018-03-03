16720aeb774813276d15ba0d7569f78107321606
igciez <36281553+igciez@users.noreply.github.com>
2018-03-02 21:14:22 -0300

    Add files via upload

--- 9-Parciales/parcial/siete.js
+++ 9-Parciales/parcial/siete.js
@@ -3,28 +3,40 @@ function Mostrar()
 	var notas;
 	var sexo;
 	var acumulador=0;
-	var contador=0;
+	var minima;
+	var cantidadvarones=0;
 
-	for(contador=1;contador<=10;contador++) {
+	for(contador=0;contador<10;contador++) {
 		notas=prompt("Ingrese la nota de '0 al 10': ");
+		if(notas==null){
+			break;
+		}
 		notas=parseInt(notas);
-		if (isNaN(notas)) {
-				continue;}
-		
-		if(notas<0&&notas>10){
-			continue;}
-		
+		while(isNaN(notas)||notas<0||notas>10) {
+			notas=prompt("Error, Reingrese la nota de '0 al 10': ");
+			notas=parseInt(notas);;
+		}
 		sexo=prompt("Ingrese 'm' o 'f': ");
+		if(sexo==null){
+			break;
+		}
+		while(sexo!="m"&&sexo!="f") {
+			sexo=prompt("Error, Reingrese 'm' o 'f': ");
+		}
 		
-		if (sexo!="m"||sexo!="f") {
-			continue;
+		if(contador==0){
+			minima=notas;
+		}
+		if (notas<minima) {
+			minima=notas;
+		}
+		if(sexo=="m"&&notas>=6){
+			cantidadvarones++;
 		}
 		acumulador+=notas;
-		if(contador==0){
-			maximo=notas
-		}
-	}
 
+	}
+	alert("Promedio de todas las notas: "+(acumulador/10) + " La nota mas baja: "+minima + " Varones que su nota haya sido mayor o igual a 6: "+cantidadvarones);
 
 }
 
